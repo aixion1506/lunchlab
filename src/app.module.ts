41510173a9fca51aabec6eb10e93ff8ab4bdc1ef
aixion1506 <aixion1506@gmail.com>
2025-03-17 17:14:59 +0900

    Chore : Sqlite 및 라이브러리 셋팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: process.env.DATABASE_PATH,
+      autoLoadEntities: true,
+      synchronize: true, // 개발용
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
